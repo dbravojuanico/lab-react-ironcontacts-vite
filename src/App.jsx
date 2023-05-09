9ee79d8145992cccb03f6dff09a821f68a134ac5
Dani Bravo <dbravojuanico@icloud.com>
2023-05-09 17:32:21 +0200

    almos done...

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newArray, setNewArray] = useState(contacts.slice(0, 5));
+  console.log(contacts);
+  console.log(newArray);
+  function selectRandomContact() {
+    let index = Math.floor(Math.random() * contacts.length);
+    console.log(contacts[index]);
+    return contacts[index];
+  }
+  console.log(newArray);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Iron Contacts</h1>
+      <button onClick={() => setNewArray([...newArray, selectRandomContact()])}>
+        Add one element
+      </button>
+      <button
+        onClick={() =>
+          setNewArray(
+            [...newArray].sort((a, b) => {
+              return b.popularity - a.popularity;
+            })
+          )
+        }
+      >
+        Sort by popularity
+      </button>
+      <button
+        onClick={() =>
+          setNewArray(
+            [...newArray].sort(function (a, b) {
+              if (a.name < b.name) {
+                return -1;
+              }
+              if (a.name > b.name) {
+                return 1;
+              }
+              return 0;
+            })
+          )
+        }
+      >
+        sort by name
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>PICTURE</th>
+            <th>NAME</th>
+            <th>POPULARITY</th>
+            <th>EMMY</th>
+            <th>OSCAR</th>
+            <th>ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {newArray.map((currentContact) => (
+            <tr name={currentContact.name}>
+              <td>
+                <img src={currentContact.pictureUrl} />
+              </td>
+              <td>{currentContact.name}</td>
+              <td>{currentContact.popularity}</td>
+              {currentContact.wonEmmy ? <td>🏆</td> : <td></td>}
+              {currentContact.wonOscar ? <td>🏆</td> : <td></td>}
+              <button>Delete</button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
